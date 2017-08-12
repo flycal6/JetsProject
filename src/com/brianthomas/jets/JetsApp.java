6f8f64349bb8440289941a1d4ed23299dfb27f9e
Brian Thomas <brianraythomas@gmail.com>
2017-08-12 14:56:38 -0600

    display menu added (user story 1)

--- src/com/brianthomas/jets/JetsApp.java
+++ src/com/brianthomas/jets/JetsApp.java
@@ -1,5 +1,7 @@
 package com.brianthomas.jets;
 
+import java.util.Scanner;
+
 public class JetsApp {
 
 	public JetsApp() {
@@ -7,8 +9,25 @@ public class JetsApp {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JetsApp ja = new JetsApp();
+		ja.displayMenu();
+	}
 
+	public int displayMenu() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Select Option: ");
+		System.out.println("****************************");
+		System.out.println("(1) List Fleet");
+		System.out.println("(2) View Fastest Jet");
+		System.out.println("(3) View Jet with Longest Range");
+		System.out.println("(4) Add a Jet to Fleet");
+		System.out.println("(5) Quit");
+
+		System.out.print(">> ");
+		int choice = sc.nextInt();
+
+		return choice;
 	}
 
 }
