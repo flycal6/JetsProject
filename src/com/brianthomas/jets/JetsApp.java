2117509b4970b60a8394e82ee62559cd5ffa90ed
Brian Thomas <brianraythomas@gmail.com>
2017-08-13 14:51:18 -0600

    added addJetToFleet(), displayMenu() now catches non-integer inputs

--- src/com/brianthomas/jets/JetsApp.java
+++ src/com/brianthomas/jets/JetsApp.java
@@ -1,43 +1,48 @@
 package com.brianthomas.jets;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class JetsApp {
+	private static Scanner sc = new Scanner(System.in);
 
 	public JetsApp() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		JetsApp ja = new JetsApp();
 
 		Jet cesna = new Jet("Cesna 172", 188, 736.3, 274_900);
-		Jet c5 = new Jet("C-5 Galaxy", 540, 6_320, 100_370_000);
+		Jet c5 = new Jet("C-5 Galaxy", 540, 16_320, 100_370_000);
 		Jet a380 = new Jet("Airbus A380", 683, 8000, 375_300_300);
 		Jet citX = new Jet("Cesna Citation X", 604, 3700, 23_365_000);
 		Jet cub = new Jet("Piper Cub J-3", 87, 220, 75_000);
 
-		Jet[] fleet = { cesna, c5, a380, citX, cub};
+		Jet[] fleet = new Jet[10];
+		fleet[0] = cesna;
+		fleet[1] = citX;
+		fleet[2] = cub;
+		fleet[3] = a380;
+		fleet[4] = c5;
+
 		ja.displayMenu(fleet);
 	}
 
 	public void displayMenu(Jet[] jets) {
-		int choice = 0;
-		Scanner sc = new Scanner(System.in);
-		do {
-	
-			System.out.println("\n\nSelect Option: ");
-			System.out.println("****************************");
-			System.out.println("(1) List Fleet");
-			System.out.println("(2) View Fastest Jet");
-			System.out.println("(3) View Jet with Longest Range");
-			System.out.println("(4) Add a Jet to Fleet");
-			System.out.println("(5) Quit");
-	
-			System.out.print(">> ");
-			choice = sc.nextInt();
-		
-			switch (choice) {
+		// the try is for when people enter something other than a number
+		try {
+			int choice = 0;
+			do {
+
+				System.out.print("\n\nSelect Option: \n" + "****************************\n" + "(1) List Fleet\n"
+						+ "(2) View Fastest Jet\n" + "(3) View Jet with Longest Range\n" + "(4) Add a Jet to Fleet\n"
+						+ "(5) Quit\n" + ">> ");
+
+				choice = sc.nextInt();
+				// necessary to consume the \n char, which is ignored by the above nextInt()
+				sc.nextLine();
+
+				switch (choice) {
 				case 1:
 					listFleet(jets);
 					break;
@@ -48,44 +53,77 @@ public class JetsApp {
 					viewLongestRangeJet(jets);
 					break;
 				case 4:
+					addJetToFleet(jets);
 					break;
 				case 5:
 					System.exit(0);
 					break;
 				default:
 					System.out.println("\n**Your input could not be read.  Try again :)**\n\n");
-//					System.exit(0);
-			}
-		} while(choice != 5);
-		sc.close();
+					// System.exit(0);
+				}
+			} while (choice != 5);
+		} catch (InputMismatchException ime) {
+			System.err.println("Please only enter integers.");
+
+			sc.nextLine();
+			displayMenu(jets);
+		}
 	}
-	
+
+	public void addJetToFleet(Jet[] jets) {
+
+		System.out.print("\nJet Model?\n>> ");
+		String model = sc.nextLine();
+
+		System.out.println("\nSpeed (mph)?\n>> ");
+		double speed = sc.nextDouble();
+
+		System.out.println("\nRange (nm)?\n>> ");
+		double range = sc.nextDouble();
+
+		System.out.println("\nPrice? \n>> ");
+		int price = sc.nextInt();
+
+		for (int i = 0; i < jets.length; i++) {
+			if (jets[i] == null) {
+				jets[i] = new Jet(model, speed, range, price);
+				break;
+			}
+		}
+		System.out.println("\n*** Jet Successfully Added ***\n");
+	}
+
 	public void viewLongestRangeJet(Jet[] jets) {
 		Jet longest = jets[0];
 		for (int i = 0; i < jets.length; i++) {
-			if (jets[i].getRange() > longest.getRange()) {
-				longest = jets[i];
+			if (jets[i] != null) {
+				if (jets[i].getRange() > longest.getRange()) {
+					longest = jets[i];
+				}
 			}
 		}
 		System.out.println("\n" + longest);
 	}
-	
+
 	public void viewFastestJet(Jet[] jets) {
 		Jet fastest = jets[0];
 		for (int i = 0; i < jets.length; i++) {
-			if(jets[i].getSpeed() > fastest.getSpeed()) {
-				fastest = jets[i];
+			if (jets[i] != null) {
+				if (jets[i].getSpeed() > fastest.getSpeed()) {
+					fastest = jets[i];
+				}
 			}
 		}
 		System.out.println("\n" + fastest);
 	}
 
 	public void listFleet(Jet[] jets) {
-		// List fleet prints out the model, speed, range, and price of each jet.
-		// (There must be at least 5 jets stored when the program starts).
 		System.out.println();
 		for (int i = 0; i < jets.length; i++) {
-			System.out.println(jets[i]);
+			if (jets[i] != null) {
+				System.out.println(jets[i]);
+			}
 		}
 	}
 
