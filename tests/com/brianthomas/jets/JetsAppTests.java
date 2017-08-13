b9ce43129827dfa3af093f3cd73e1085bfe66e79
Brian Thomas <brianraythomas@gmail.com>
2017-08-13 15:36:27 -0600

    JUnit testing set up

--- tests/com/brianthomas/jets/JetsAppTests.java
+++ tests/com/brianthomas/jets/JetsAppTests.java
@@ -7,18 +7,24 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class JetsAppTests {
-
+	Pilot pilot;
+	Jet jet;
+	
 	@Before
 	public void setUp() throws Exception {
+		pilot = new Pilot("TestPilot", 33, 12);
+		jet = new Jet("Test Jet", 555, 2500, 250_999, pilot);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		pilot = null;
+		jet = null;
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals(pilot.getName(), "TestPilot");
 	}
 
 }
